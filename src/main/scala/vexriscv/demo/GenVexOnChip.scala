9271dd1c3c1acfa47c1bafd4de4a2ca8e355223d
Chiro Liang <Chiro2001@163.com>
2022-10-30 23:30:16 +0800

    vexriscv: use full axi4

--- src/main/scala/vexriscv/demo/GenVexOnChip.scala
+++ src/main/scala/vexriscv/demo/GenVexOnChip.scala
@@ -1,17 +1,12 @@
 package vexriscv.demo
 
 import spinal.core._
-import spinal.lib.bus.amba3.apb.{Apb3, Apb3Config, Apb3Decoder, Apb3Gpio}
 import spinal.lib.bus.amba4.axi.{Axi4CrossbarFactory, Axi4ReadOnly, Axi4Shared, Axi4SharedOnChipRam}
-import spinal.lib.bus.misc.SizeMapping
-import spinal.lib.bus.simple.{PipelinedMemoryBus, PipelinedMemoryBusConfig, PipelinedMemoryBusToApbBridge}
 import spinal.lib.com.jtag.Jtag
-import spinal.lib.com.spi.ddr.{Apb3SpiXdrMasterCtrl, SpiXdrMaster, SpiXdrMasterCtrl, SpiXdrParameter}
-import spinal.lib.com.uart.{Apb3UartCtrl, Uart, UartCtrlGenerics, UartCtrlInitConfig, UartCtrlMemoryMappedConfig, UartParityType, UartStopType}
-import spinal.lib.io.TriStateArray
-import spinal.lib.{BufferCC, master, slave}
-import vexriscv.{VexRiscv, VexRiscvConfig, plugin}
+import spinal.lib.eda.altera.{InterruptReceiverTag, ResetEmitterTag}
+import spinal.lib.{master, slave}
 import vexriscv.plugin._
+import vexriscv.{VexRiscv, VexRiscvConfig, plugin}
 
 import scala.collection.mutable.ArrayBuffer
 import scala.language.postfixOps
@@ -23,7 +18,7 @@ case class VexOnChipConfig
  cpuPlugins: ArrayBuffer[Plugin[VexRiscv]]) {
 }
 
-import VexInterfaceConfig._
+import vexriscv.demo.VexInterfaceConfig._
 
 object VexOnChipConfig {
   def default: VexOnChipConfig = default(false)
@@ -96,91 +91,64 @@ object VexOnChipConfig {
 }
 
 case class VexOnChip(config: VexOnChipConfig) extends Component {
-
   import config._
 
-  val io = new Bundle {
-    //Clocks / reset
-    val reset = in Bool()
-    val clk = in Bool()
+  //Clocks / reset
+  val reset = in Bool()
+  val clk = in Bool()
 
-    //Main components IO
-    val jtag = slave(Jtag())
-
-    // val iBus = master(Axi4ReadOnly(IBusSimpleBus.getAxi4Config()))
-    // val dBus = master(Axi4Shared(DBusSimpleBus.getAxi4Config()))
-    var iBus: Axi4ReadOnly = _
-    var dBus: Axi4Shared = _
-  }
-
-
-  val resetCtrlClockDomain = ClockDomain(
-    clock = io.clk,
-    config = ClockDomainConfig(
-      resetKind = BOOT
+  // Instantiate the CPU
+  val cpu = new VexRiscv(
+    config = VexRiscvConfig(
+      plugins = cpuPlugins += new DebugPlugin(ClockDomain.current.copy(reset = Bool().setName("debugReset")))
     )
   )
 
-  val resetCtrl = new ClockingArea(resetCtrlClockDomain) {
-    val mainClkResetUnbuffered = False
-
-    //Implement an counter to keep the reset axiResetOrder high 64 cycles
-    // Also this counter will automatically do a reset when the system boot.
-    val systemClkResetCounter = Reg(UInt(6 bits)) init (0)
-    when(systemClkResetCounter =/= U(systemClkResetCounter.range -> true)) {
-      systemClkResetCounter := systemClkResetCounter + 1
-      mainClkResetUnbuffered := True
-    }
-    when(BufferCC(io.reset)) {
-      systemClkResetCounter := 0
-    }
-
-    //Create all reset used later in the design
-    val mainClkReset = RegNext(mainClkResetUnbuffered)
-    val systemReset = RegNext(mainClkResetUnbuffered)
-  }
-
-
-  val systemClockDomain = ClockDomain(
-    clock = io.clk,
-    reset = resetCtrl.systemReset
-  )
-
-  val debugClockDomain = ClockDomain(
-    clock = io.clk,
-    reset = resetCtrl.mainClkReset
-  )
-
-  val core = new ClockingArea(systemClockDomain) {
-    // Instantiate the CPU
-    val cpu = new VexRiscv(
-      config = VexRiscvConfig(
-        plugins = cpuPlugins += new DebugPlugin(debugClockDomain, hardwareBreakpointCount)
-      )
-    )
-
-    //Checkout plugins used to instanciate the CPU to connect them to the SoC
-    val timerInterrupt = False
-    val externalInterrupt = False
-    var iBus: Axi4ReadOnly = _
-    var dBus: Axi4Shared = _
+  cpu.rework {
+    var iBus: Axi4ReadOnly = null
+    var dBus: Axi4Shared = null
+    // var dBus: Axi4 = null
     for (plugin <- cpu.plugins) plugin match {
-      case plugin: IBusSimplePlugin => iBus = plugin.iBus.toAxi4ReadOnly().toFullConfig()
-      case plugin: IBusCachedPlugin => iBus = plugin.iBus.toAxi4ReadOnly().toFullConfig()
-      case plugin: DBusSimplePlugin => dBus = plugin.dBus.toAxi4Shared().toFullConfig()
-      case plugin: DBusCachedPlugin => dBus = plugin.dBus.toAxi4Shared(stageCmd = true).toFullConfig()
-      case plugin: CsrPlugin =>
-        plugin.externalInterrupt := externalInterrupt
-        plugin.timerInterrupt := timerInterrupt
-      case plugin: DebugPlugin => plugin.debugClockDomain {
-        resetCtrl.systemReset setWhen (RegNext(plugin.io.resetOut))
-        io.jtag <> plugin.io.bus.fromJtag()
+      case plugin: IBusSimplePlugin => {
+        plugin.iBus.setAsDirectionLess() //Unset IO properties of iBus
+        iBus = master(plugin.iBus.toAxi4ReadOnly().toFullConfig())
+          .setName("iBus")
+          .addTag(ClockDomainTag(ClockDomain.current)) //Specify a clock domain to the iBus (used by QSysify)
       }
-
+      case plugin: IBusCachedPlugin => {
+        plugin.iBus.setAsDirectionLess() //Unset IO properties of iBus
+        iBus = master(plugin.iBus.toAxi4ReadOnly().toFullConfig())
+          .setName("iBus")
+          .addTag(ClockDomainTag(ClockDomain.current)) //Specify a clock domain to the iBus (used by QSysify)
+      }
+      case plugin: DBusSimplePlugin =>
+        plugin.dBus.setAsDirectionLess()
+        dBus = plugin.dBus.toAxi4Shared().toFullConfig()
+      case plugin: DBusCachedPlugin =>
+        plugin.dBus.setAsDirectionLess()
+        dBus = plugin.dBus.toAxi4Shared(stageCmd = true).toFullConfig()
+      case plugin: DebugPlugin => plugin.debugClockDomain {
+        plugin.io.bus.setAsDirectionLess()
+        val jtag = slave(new Jtag())
+          .setName("jtag")
+        jtag <> plugin.io.bus.fromJtag()
+        plugin.io.resetOut
+          .addTag(ResetEmitterTag(plugin.debugClockDomain))
+          .parent = null //Avoid the io bundle to be interpreted as a QSys conduit
+      }
+      case _ =>
+    }
+    for (plugin <- cpu.plugins) plugin match {
+      case plugin: CsrPlugin => {
+        plugin.externalInterrupt
+          .addTag(InterruptReceiverTag(iBus, ClockDomain.current))
+        plugin.timerInterrupt
+          .addTag(InterruptReceiverTag(iBus, ClockDomain.current))
+      }
       case _ =>
     }
 
-    io.dBus = master(dBus.copy()).setName("dBus")
+    val reqBus = dBus.copy()
 
     val ram = Axi4SharedOnChipRam(
       dataWidth = 32,
@@ -193,11 +161,11 @@ case class VexOnChip(config: VexOnChipConfig) extends Component {
 
     axiCrossbar.addSlaves(
       ram.io.axi -> (0x80000000L, onChipRamSize),
-      io.dBus -> (0x00000000L, BigInt(0x80000000L))
+      reqBus -> (0x00000000L, BigInt(0x80000000L))
     )
 
     axiCrossbar.addConnections(
-      dBus -> List(ram.io.axi, io.dBus)
+      dBus -> List(ram.io.axi, reqBus)
     )
 
     axiCrossbar.addPipelining(ram.io.axi)((crossbar, ctrl) => {
@@ -207,7 +175,7 @@ case class VexOnChip(config: VexOnChipConfig) extends Component {
       crossbar.readRsp << ctrl.readRsp
     })
 
-    axiCrossbar.addPipelining(dBus)((cpu, crossbar) => {
+    axiCrossbar.addPipelining(reqBus)((cpu, crossbar) => {
       cpu.sharedCmd >> crossbar.sharedCmd
       cpu.writeData >> crossbar.writeData
       cpu.writeRsp << crossbar.writeRsp
@@ -216,14 +184,13 @@ case class VexOnChip(config: VexOnChipConfig) extends Component {
 
     axiCrossbar.build()
 
-    io.iBus = master(iBus.copy()).setName("iBus")
-    io.iBus <> iBus.toFullConfig()
+    val reqBusFull = master(reqBus.toAxi4().setName("dBus"))
   }
 }
 
 object GenVexOnChip {
   def main(args: Array[String]): Unit = {
-    val name = if(args.size == 0) "VexCore" else args(0)
+    val name = if (args.size == 0) "VexCore" else args(0)
     SpinalVerilog(VexOnChip(VexOnChipConfig.default).setDefinitionName(name))
   }
 }
